ca857dc144a28e044f8198d5324449e08972d55b
Tim <t.reinders2@gmail.com>
2025-01-16 12:09:06 +0100

    feat: extension method to assert an exception was thrown

---
+++ src/Shouldify/ExceptionExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Shouldify;
+
+public static class ExceptionExtensions
+{
+    public static void ShouldThrow<TException>(this Action action, string? message = null)
+        where TException : Exception
+    {
+        try
+        {
+            action();
+        }
+        catch (TException exception) when (message is null || exception.Message == message)
+        {
+            return;
+        }
+        catch (Exception exception)
+        {
+            Shouldify.Throw($"Expected '{typeof(TException).Name}' to be thrown, but '{exception.GetType().Name}' was thrown.");
+        }
+
+        Shouldify.Throw("Expected '{typeof(TException).Name}', but no exception was thrown.");
+    }
+}
